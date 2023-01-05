f4739d7d5e96ed7d09de8f040161470167985a39
Alexandr Korobov <devarko@sapo.pt>
2023-01-05 19:46:25 +0300

    News home view & route

--- blog/settings.py
+++ blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--qfe90=u=swvy=eoz$@7)oi(vsup7nb5rpdt1akxg^0=wdctg='
+from env.vars import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # Blog apps
     'news',
 ]
 
